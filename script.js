c5d948df9cbea63969fa118bcbe12def0b946360
xxRyuka <146874230+xxRyuka@users.noreply.github.com>
2024-09-02 01:52:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-console.log("DİCK")
-
-
-
-window.addEventListener('load', () => {
-    // 5 saniyelik gecikme
-    setTimeout(() => {
-        const loading = document.getElementById('loading');
-        const content = document.getElementById('content');
-
-        // Yükleme sayfasını gizle ve içerikleri göster
-        loading.style.display = 'none';
-        content.style.display = 'block';
-    }, 2000); // 5000 milisaniye = 5 saniye
-});
-
 // Sayfa yüklendiğinde çalışacak kod
 window.onload = function() {
     // localStorage'da 'pageRefreshed' adında bir anahtar olup olmadığını kontrol et
@@ -28,5 +12,19 @@ window.onload = function() {
 };
 
 
-//
+window.addEventListener('load', () => {
+    // 5 saniyelik gecikme
+    setTimeout(() => {
+        const loading = document.getElementById('loading');
+        const content = document.getElementById('content');
+
+        // Yükleme sayfasını gizle ve içerikleri göster
+        loading.style.display = 'none';
+        content.style.display = 'block';
+    }, 2000); // 5000 milisaniye = 5 saniye
+});
+
+
+
+console.log("work")
 
